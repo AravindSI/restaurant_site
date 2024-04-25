f0b0971116549b15c55ed6d83b1a6ce63037672b
AravindSI <aravindsivankalai99>
2024-04-25 16:40:55 +0530

    Upto Card

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = [];
+  return (
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Cart;
